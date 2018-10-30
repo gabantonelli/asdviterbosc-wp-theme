39498e5169275838edcb2d466ef557d6fdb123c3
Gabriel Antonelli <gabrielantonelli@hotmail.com>
2018-10-30 19:17:01 +0100

    added articles loop to the index homepage, and support for featured images

--- functions.php
+++ functions.php
@@ -11,3 +11,10 @@ function theme_setup(){
 }
 
 add_action('after_setup_theme', 'theme_setup' );
+
+// Excerpt lenght
+function set_excerpt_length(){
+	return 40;
+}
+
+add_filter('excerpt_length', 'set_excerpt_length');
