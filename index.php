109abfc1d83cbb9ff068d4352798ef95733f7a5e
Gabriel Antonelli <gabrielantonelli@hotmail.com>
2018-10-30 10:57:58 +0100

    creata la pagine base index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="it">
+    <head>
+        <title>titolo</title>
+        <meta name="author" content="Gabriel Antonelli">
+        <meta name="description" content="ASD Scuola Calcio Viterbo">
+    </head>
+    <body>
+        
+    </body>
+</html>
