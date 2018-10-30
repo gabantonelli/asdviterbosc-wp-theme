1c2a389b713b4c397b2d191fcb4efd45357a59fe
Gabriel Antonelli <gabrielantonelli@hotmail.com>
2018-10-30 18:28:37 +0100

    added wp-bootstrapp-navwalker to generate bootstrap dropdown menus

--- header.php
+++ header.php
@@ -12,3 +12,23 @@
         <?php wp_head(); ?>
     </head>
     <body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <?php
+            wp_nav_menu( array(
+                'theme_location'  => 'primary',
+                'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
+                'container'       => 'div',
+                'container_class' => 'collapse navbar-collapse',
+                'container_id'    => 'bs-example-navbar-collapse-1',
+                'menu_class'      => 'navbar-nav mr-auto',
+                'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'          => new WP_Bootstrap_Navwalker(),
+            ) );
+            ?>
+        </nav>
+    </div>
