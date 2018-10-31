2d9b68c088964c35dc87059a709913e8df211f60
Gabriel Antonelli <gabrielantonelli@hotmail.com>
2018-10-31 17:17:26 +0100

    added support for sidebar widgets

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 // Theme support
 function theme_setup(){
 	//support featured images
-	add_theme_support('post-thumbnails')
+	add_theme_support('post-thumbnails');
 
 	//nav menus
 	register_nav_menus( array(
@@ -21,3 +21,17 @@ function set_excerpt_length(){
 }
 
 add_filter('excerpt_length', 'set_excerpt_length');
+
+// Widget locations
+function scvt_init_widgets($id){
+	register_sidebar(array(
+		'name' => 'Sidebar',
+		'id' => 'sidebar',
+		'before_widget' => '<div class="sidebar-module">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4>',
+		'after_title' => '</h4>'
+	));
+}
+
+add_action('widgets_init', 'scvt_init_widgets');
