a859c8a7a2244fc12e3c8fe814eba0c2f2bfa2c5
Gabriel Antonelli <gabrielantonelli@hotmail.com>
2018-11-15 08:29:56 +0100

    fixed the grid of articles in the homepage for few posts

--- index.php
+++ index.php
@@ -34,60 +34,68 @@
         </a>
       </div>
       <main class="container">
-      <section>
+        <section>
           <?php if(have_posts()) : $contatorePost = 0;?>
             <?php while(have_posts()&&$contatorePost<=5): the_post();?>
             <?php 
             //apro la row di notizie
-            if($contatorePost % 3 == 0) :?>
+            if($contatorePost % 3 == 0) :
+            $postRimanentiFila = 3;
+            ?>
               <div class="row">
             <?php endif; ?>
             <div class="col-md-4">
             <article class="home-preview">
-  <header>
-      <h2>
-    <?php if(is_single()): ?>
-      <?php the_title(); ?>
-    <?php else : ?>
-      <a href="<?php the_permalink();?>"><?php the_title(); ?></a>
-    <?php endif;?>
-    </h2>
-    <div class="article-meta">
-      <p><?php the_time('F j, Y g:i a');?> da
-      <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
-        <?php the_author()?></p>
-      </a>
-    </div>
-</header>
-<a href="<?php the_permalink();?>">
-<?php if(has_post_thumbnail()) :?>
-<?php the_post_thumbnail('home-thumb', array(
+            <header>
+              <h2>
+                <a href="<?php the_permalink();?>"><?php the_title(); ?></a>
+              </h2>
+              <div class="article-meta">
+                <p><?php the_time('F j, Y g:i a');?> da
+                <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+                  <?php the_author()?></p>
+                </a>
+              </div>
+            </header>
+            <a href="<?php the_permalink();?>">
+              <?php if(has_post_thumbnail()) :?>
+              <?php the_post_thumbnail('home-thumb', array(
                          'class' => 'img-fluid preview-immagine',
                          'alt' => get_the_title())
                          ); ?>
-<?php else: ?>
-<img class="img-fluid preview-immagine" src="<?php bloginfo('template_directory');?>/img/asd-viterbo-scuola-calcio-no-foto.png" alt="Nessuna immagine per il post">
-<?php endif; ?>
-</a>
-  <div class="preview-articolo">
-    <a href="<?php the_permalink();?>">
-      <?php the_excerpt(); ?>
-      <button class="btn btn-primary btn-sm">Leggi</button>
-    </a>  
-</div>
-</article>
+              <?php else: ?>
+                <img class="img-fluid preview-immagine" src="<?php bloginfo('template_directory');?>/img/asd-viterbo-scuola-calcio-no-foto.png" alt="Nessuna immagine per il post">
+              <?php endif; ?>
+              </a>
+              <div class="preview-articolo">
+                <a href="<?php the_permalink();?>">
+                  <?php the_excerpt(); ?>
+                  <button class="btn btn-primary btn-sm">Leggi</button>
+                </a>
+              </div>
+            </article>
             </div>   
-            <?php $contatorePost++; ?>
+            <?php $contatorePost++;
+            $postRimanentiFila--;
+            if ($postRimanentiFila == 0):
+            // se sono finiti i post nella fila chiudo la row?>
+            </div>
+            <?php endif;?>
             <?php endwhile; ?>
             <?php 
-            // chiudo la linea di notizie
-            if($contatorePost % 3 == 0) :?>
+            // controllo se bisogna aggiungere div vuoti
+            if ($postRimanentiFila>0) :
+            while ($postRimanentiFila>0) :?>
+            <div class="col-md-4">&nbsp;</div>
+            <?php
+            $postRimanentiFila--;
+            endwhile;?>
             </div>
             <?php endif; ?> 
             <?php else: ?>
               <p><?php __('No Posts Found'); ?></p>
             <?php endif; ?>
-      </main>
-    </section>
+    </main>
+  </section>
     
 <?php get_footer(); ?>
